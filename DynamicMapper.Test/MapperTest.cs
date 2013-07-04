06c3e941576901f475c00be465ed5c10b5dbd4e6
manueldiego <chico_herbal@hotmail.com>
2013-07-04 07:07:48 +0200

    new implementations of default dynamic mappers.

--- DynamicMapper.Test/MapperTest.cs
+++ DynamicMapper.Test/MapperTest.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
-using AutoMapper;
 using NUnit.Framework;
 
 namespace DynamicMapper.Test
@@ -14,24 +14,93 @@ namespace DynamicMapper.Test
         [Test]
         public void Test1()
         {
-            Type t = typeof (Person);
-            var properties = t.GetProperties();
+            var studente = new Student();
+            studente.Name = "ciccio";
+
+            var persona = new Person();
+            
+
+            Type TSource = typeof(Student);
+            Type TDestination = typeof(Person);
+
+            Func<Student, string> cc = (Func<Student, string>)Delegate.CreateDelegate(typeof(Func<Student, string>), null, TSource.GetProperty("Name").GetGetMethod());
+            Action<Person, string> dd = (Action<Person, string>)Delegate.CreateDelegate(typeof(Action<Person, string>), null, TDestination.GetProperty("Name").GetSetMethod());
+            
+
+            Action<Student, Person> azione = (student, person) => dd.Invoke(person, cc.Invoke(student));
+
+            Assert.AreNotEqual(studente.Name, persona.Name);
+
+            azione.Invoke(studente, persona);
+
+            Assert.AreEqual(studente.Name, persona.Name);
+
+            Console.WriteLine(cc.ToString());
+            Console.WriteLine(dd.ToString());
+            
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Type funcType = typeof (Func<,>);
+            Type ActType = typeof (Action<,>);
+
+            Type TSource = typeof (Student);
+            Type TDest = typeof (Person);
+
+            var studente = new Student();
+            studente.Name = "ciccio";
+
+            var persona = new Person();
 
 
+            string nameProperty = "Name";
+            PropertyInfo propSource = TSource.GetProperty(nameProperty);
+            PropertyInfo propDest = TDest.GetProperty(nameProperty);
+
+            funcType = funcType.MakeGenericType(TSource, propSource.PropertyType);
+            ActType = ActType.MakeGenericType(TDest, propDest.PropertyType);
+
+            Delegate getter = Delegate.CreateDelegate(funcType, null, propSource.GetGetMethod());
+            Delegate setter = Delegate.CreateDelegate(ActType, null, propDest.GetSetMethod());
+
+            Action<Student, Person> azione = (student, person) => setter.DynamicInvoke(person, getter.DynamicInvoke(student));
+
+            Assert.AreNotEqual(studente.Name, persona.Name);
+
+            azione.Invoke(studente, persona);
+
+            Assert.AreEqual(studente.Name, persona.Name);
+
+            Console.WriteLine("ciao");
+
+            
 
 
         }
+
     }
 
     public class Person
     {
         public string Name { get; set; }
         public string Surname { get; set; }
+        public long AnnoNascita { get; set; }
+    }
+
+    public class PersonaGiuridica
+        : Person
+    {
+        public string Code { get; set; }
     }
 
     public class Student
     {
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int AnnoNascita { get; set; }
     }
 }
--- DynamicMapper/FactoryMapper.cs
+++ DynamicMapper/FactoryMapper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace DynamicMapper
@@ -10,13 +11,66 @@ namespace DynamicMapper
     /// </summary>
     public class FactoryMapper
     {
+        private IDictionary<Type, HashSet<Type>> primiteTypes;
+
+        static FactoryMapper()
+        {
+            //Dictionary<Type, HashSet<Type>> primiteTypes = new Dictionary<Type, HashSet<Type>>();
+
+            //Type currentKey;
+            //HashSet<Type> col;
+
+            //currentKey = typeof (byte);
+            //col = new HashSet<Type>();
+            //col.Add(currentKey);
+            //primiteTypes.Add(currentKey, col);
+
+            //currentKey = typeof(byte?);
+            //col = new HashSet<Type>(col);
+            //col.Add(currentKey);
+            //primiteTypes.Add(currentKey, col);
+
+
+
+            //currentKey = typeof(short);
+            //col = new HashSet<Type>();
+            //col.Add(typeof (byte));
+            //col.Add(currentKey);
+            //primiteTypes.Add(currentKey, col);
+
+            //currentKey = typeof(short?);
+            //col = new HashSet<Type>(col);
+            //col.Add(typeof(byte?));
+            //col.Add(currentKey);
+            //primiteTypes.Add(currentKey, col);
+
+
+
+            //currentKey = typeof(int);
+            //col = new HashSet<Type>();
+            //col.Add(typeof(byte));
+            //col.Add(typeof (short));
+            //col.Add(currentKey);
+            //primiteTypes.Add(currentKey, col);
+
+            //currentKey = typeof(int?);
+            //col = new HashSet<Type>(col);
+            //col.Add(typeof(byte?));
+            //col.Add(typeof(short?));
+            //col.Add(currentKey);
+            //primiteTypes.Add(currentKey, col);
+
+
+
+        }
+
         /// <summary>
         /// 
         /// </summary>
         /// <typeparam name="TSource"></typeparam>
         /// <typeparam name="TDestination"></typeparam>
         /// <returns></returns>
-        public static ISourceMapper<TSource, TDestination> CreateMapper<TSource, TDestination>()
+        public static ISourceMapper<TSource, TDestination> DynamicResolutionMapper<TSource, TDestination>()
             where TSource: class
             where TDestination: class, new()
         {
@@ -29,11 +83,29 @@ namespace DynamicMapper
         /// <typeparam name="TSource"></typeparam>
         /// <typeparam name="TDestination"></typeparam>
         /// <returns></returns>
-        public static ISourceMerger<TSource, TDestination> CreateMerger<TSource, TDestination>()
+        public static ISourceMerger<TSource, TDestination> DynamicResolutionMerger<TSource, TDestination>()
             where TSource : class
             where TDestination : class
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TCurrent"></typeparam>
+        /// <returns></returns>
+        public static PropertyInfo[] GetPropertiesOf<TCurrent>()
+        {
+            return typeof (TCurrent).GetProperties();
+        }
+
+        public static IDictionary<PropertyInfo, PropertyInfo> GetSuitedPropertiesOf<TSource, TDestination>()
+        {
+            PropertyInfo[] source = GetPropertiesOf<TSource>();
+
+
+            return null;
         }
     }
 }
